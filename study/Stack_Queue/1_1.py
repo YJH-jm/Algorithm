1c63393c78dcd962f15668ef21768bed357ff925
YJH-jm <jmjhjob13@gmail.com>
2021-11-15 21:11:46 +0900

    update

--- study/Stack_Queue/1_1.py
+++ study/Stack_Queue/1_1.py
@@ -18,3 +18,43 @@ def solution(prices):
             answer.append(sec)
         
     return answer
+
+
+def solution2(prices): # O(n^2) -> 시간 줄일 수 있는 풀이 생각해 볼 것 ....
+    answer = []
+    
+    for i in range(len(prices)-1):
+        cnt = 0
+        for j in range(i+1, len(prices)):
+            cnt+=1
+            if prices[i] > prices[j]:
+                break
+        answer.append(cnt)
+    
+    answer.append(0)
+    
+    return answer
+
+def solution3(prices): # stack풀이 
+    len_prices = len(prices)
+    answer = [0] * len_prices
+    
+    stack = []
+    
+    for i in range(len_prices):
+        # 1. 스택 비지 않고, prices[top] > prices[i] 이라면 다음 반복
+        # 1-1. 스택에서 마지막에 저장된 시간 top 꺼냄
+        # 1-2. answer[top]에 i - top을 저장
+        while stack and prices[stack[-1]] > prices[i]:
+            top = stack.pop()
+            answer[top] = i - top
+        # 2. 스택에 현재 시간 i 저장
+        stack.append(i)
+    
+    # 4. 만약 스택이 남아있다면, 스택이 빌 때까지 다음 반복
+    while stack:
+
+        top = stack.pop()
+        answer[top] = len_prices - 1 - top
+    
+    return answer
