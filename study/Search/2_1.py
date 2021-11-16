6091b39494762558c8b18f9fb83662c93da9067b
YJH-jm <jmjhjob13@gmail.com>
2021-11-16 23:42:48 +0900

    update 2_1.py

--- study/Search/2_1.py
+++ study/Search/2_1.py
@@ -43,5 +43,89 @@ def solution(answers):
             answer.append(k)
     answer.sort()
     return answer
-a = solution([1,3,2,4,2])
-print(a)
+
+
+def solution2(answers):
+    list1 = [1, 2, 3, 4, 5]
+    list2 = [2, 1, 2, 3, 2, 4, 2, 5]
+    list3 = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]
+
+    len_ans = len(answers)
+    len_list1 = len(list1)
+    len_list2 = len(list2)
+    len_list3  = len(list3)
+
+    index = 0
+    cnt = 0
+    while cnt < len_ans:
+        list1.append(list1[index])
+        index += 1
+        cnt +=1
+        if index == len_list1:
+            index = 0
+
+    index = 0
+    cnt = 0
+    while cnt < len_ans:
+        list2.append(list2[index])
+        index += 1
+        cnt +=1
+        if index == len_list2:
+            index = 0
+
+    index = 0
+    cnt = 0
+    while cnt < len_ans:
+        list3.append(list3[index])
+        index += 1
+        cnt +=1
+        if index == len_list3:
+            index = 0
+
+
+    dict_cnt = {1:0, 2:0, 3:0}
+
+    for i in range(len_ans):
+        if answers[i] == list1[i]:
+            dict_cnt[1] += 1
+        if answers[i] == list2[i]:
+            dict_cnt[2] += 1
+        if answers[i] == list3[i]:
+            dict_cnt[3] += 1
+ 
+    new_list = sorted(dict_cnt.values(), reverse=True)
+    answer = []
+    for k, v in dict_cnt.items():
+        if v == new_list[0]:
+            answer.append(k)
+            
+    return answer
+    
+
+def solution3(answers): # 더 간단한풀이, enumerate 사용할 것!
+    list1 = [1, 2, 3, 4, 5]
+    list2 = [2, 1, 2, 3, 2, 4, 2, 5]
+    list3 = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]
+    score = [0, 0, 0]
+    answer = []
+    
+    for idx, val in enumerate(answers):
+        if answers[idx] == list1[idx % len(list1)]:
+            score[0] += 1
+        if answers[idx] == list2[idx % len(list2)]:
+            score[1] += 1
+        if answers[idx] == list3[idx % len(list3)]:
+            score[2] += 1
+
+    for v in range(len(score)):
+        if score[v] == max(score):
+            answer.append(v+1)
+        
+    return answer
+
+if __name__ == "__main__":
+    answers = [1, 2, 3, 4, 5]
+    print(answers)
+    
+    print(solution(answers))
+    
